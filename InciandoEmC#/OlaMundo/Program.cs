539dfe827e89a891d27b5c5e2484a94e945ff884
Arthur Terozendi <arthurterozendi@gmail.com>
2020-10-06 15:42:46 -0300

    iniciando em c#

---
+++ InciandoEmC#/OlaMundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string nome = Console.ReadLine();
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Acabamos de conhecer {nome}, que está com {idade} anos");
+        }
+    }
+}
