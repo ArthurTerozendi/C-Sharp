8bc50191c08a200e1545391ad90f262e0b5e7c53
Arthur Terozendi <arthurterozendi@gmail.com>
2020-10-06 16:16:07 -0300

    calculadora simples em c#

---
+++ InciandoEmC#/PrimeirosProgramas/CalculadoraSimples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class CalculadoraSimples
+{
+	public double Somar (double num1, double num2) {
+		return num1 + num2;
+	}
+
+	public double Subtrair (double num1, double num2) {
+		return num1 - num2;
+	}
+
+	public double Multiplicar (double num1, double num2) {
+		return num1 * num2;
+	}
+
+	public double Dividir (double num1, double num2) {
+		return (num1/num2);
+    }
+}
---
+++ InciandoEmC#/PrimeirosProgramas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PrimeirosProgramas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * Calculadora simples que aceita somente dois números
+             */
+            CalculadoraSimples calculadoraSimples = new CalculadoraSimples();
+            Random rng = new Random();
+            double num1 = Convert.ToDouble(rng.Next(10));
+            double num2 = Convert.ToDouble(rng.Next(10));
+
+            Console.WriteLine(calculadoraSimples.Somar(num1, num2));
+            Console.WriteLine(calculadoraSimples.Subtrair(num1, num2));
+            Console.WriteLine(calculadoraSimples.Multiplicar(num1, num2));
+            Console.WriteLine(calculadoraSimples.Dividir(num1, num2));
+        }
+    }
+}
